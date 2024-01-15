113063beb71b4e20b67743c466c26e7b86711e88
Emanuelle <102062744+Hell0Manu@users.noreply.github.com>
2024-01-14 22:08:17 -0300

    validações com alguns erros

--- index.js
+++ index.js
@@ -2,20 +2,44 @@
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    checkInputName()
     checkInputEmail()
 })
 
 /* name validate */
+const username = document.getElementById("name");
+const warning = document.getElementById("form-warning-wrapper")
+
+function checkInputName(){
+    const usernameValue = username.value;
+
+    if(usernameValue === "") {
+        warning.style.display = 'block';
+        errorInput(warning, "Please enter a valid email address.")
+    } else{
+        warning.style.display = 'none';
+    }
+}
+
+const p = warning.querySelector("p")
+p.innerText = 'Sua nova mensagem aqui';
+
 
 
 /* Email validate */
 const email = document.getElementById("email");
 
+email.addEventListener("keyup", () => {
+    checkInputEmail();
+})
+
 function checkInputEmail(){
     const emailValue = email.value;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if(emailValue === "") {
+    if(!emailPattern.test(emailValue)) {
         errorInput(email, "Please enter a valid email address.")
+        formItem.className = "form-group error";
     } else{
         const formItem = email.parentElement;
         formItem.className = "form-group"
